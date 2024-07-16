565cc5eed8969df624b2dd3417f87cb10c5a3e3d
安岡 <2237005.ecc.ac.jp>
2024-07-16 13:29:47 +0900

    改修回転スクリプト

--- Assets/Script/Kaiten.cs
+++ Assets/Script/Kaiten.cs
@@ -6,6 +6,7 @@ public class Kaiten : MonoBehaviour
 {
     public float rotationSpeed = 1.0f;
     public float Anglez = 0;
+    public float def = 0.0f;
     public float maxAngle = 45.0f;
 
     // Start is called before the first frame update
@@ -17,10 +18,10 @@ public class Kaiten : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+            Vector3 worldAngle = transform.eulerAngles;
         if (Input.GetKey(KeyCode.RightArrow))
         {
 
-            Vector3 worldAngle = transform.eulerAngles;
             // Z軸を中心に回転させる
             Anglez -= rotationSpeed;
             if (Anglez < -maxAngle)
@@ -28,13 +29,10 @@ public class Kaiten : MonoBehaviour
                 Anglez = -maxAngle;
 
             }
-            worldAngle.z = Anglez;
-            transform.eulerAngles = worldAngle;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
 
-            Vector3 worldAngle = transform.eulerAngles;
             // Z軸を中心に回転させる
             Anglez += rotationSpeed;
             if (Anglez > maxAngle)
@@ -42,21 +40,42 @@ public class Kaiten : MonoBehaviour
                 Anglez = maxAngle;
 
             }
-            worldAngle.z = Anglez;
-            transform.eulerAngles = worldAngle;
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        else
         {
-            // transformを取得
-            Transform myTransform = this.transform;
+            Anglez *= 0.99f;
 
-            // ローカル座標を基準に、回転を取得
-            Vector3 localAngle = myTransform.localEulerAngles;
-            localAngle.x = 0.0f; // ローカル座標を基準にした、x軸を軸にした回転角度
-            localAngle.y = 0.0f; // ローカル座標を基準にした、y軸を軸にした回転角度
-            localAngle.z = 0.0f; // ローカル座標を基準にした、z軸を軸にした回転角度
-            myTransform.localEulerAngles = localAngle; // 回転角度を設定
         }
+        worldAngle.z = Anglez;
+        transform.eulerAngles = worldAngle;
+
+        //else if (Anglez > def)
+        //{
+        //    Vector3 worldAngle = transform.eulerAngles;
+        //    Anglez += rotationSpeed;
+        //    //if (Anglez < def)
+        //    //{
+        //    //    Anglez = def;
+        //    //}
+
+        //    worldAngle.z = Anglez;
+        //    transform.eulerAngles = worldAngle;
+
+
+        //}
+
+        //if(Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    // transformを取得
+        //    Transform myTransform = this.transform;
+
+        //    // ローカル座標を基準に、回転を取得
+        //    Vector3 localAngle = myTransform.localEulerAngles;
+        //    localAngle.x = 0.0f; // ローカル座標を基準にした、x軸を軸にした回転角度
+        //    localAngle.y = 0.0f; // ローカル座標を基準にした、y軸を軸にした回転角度
+        //    localAngle.z = 0.0f; // ローカル座標を基準にした、z軸を軸にした回転角度
+        //    myTransform.localEulerAngles = localAngle; // 回転角度を設定
+        //}
 
 
         //// transformを取得
